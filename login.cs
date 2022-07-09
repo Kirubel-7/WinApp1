b09823ee5d7c9b76859280b6b935fda20698b742
Kirubel Esayas <107740002+Kirubel-7@users.noreply.github.com>
2022-07-10 00:00:48 +0300

    w1

---
+++ login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Win1.loginf;
+namespace Win1
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Add add = new Add();
+            add.number = Convert.ToInt32(num.Text);
+            add.date = Convert.ToDateTime(date.Text);
+            add.invnumber = Convert.ToInt32(invnum.Text);
+            add.objname = objname.Text;
+            add.count = Convert.ToInt32(count.Text);    
+            add.price = Convert.ToDouble(price.Text);
+            MessageBox.Show(add.save());
+            
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ loginf/Add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win1.loginf
+{
+    internal class Add
+    {
+        public int number { get; set; }   
+        public DateTime date { get; set; }
+
+        public int invnumber { get; set; }
+
+        public string objname{ get; set; }
+
+        public int count { get; set; }
+
+        public double price { get; set; }
+        public string save()
+        {
+             return $"Item Has been Saved:\n{objname}\n{price}\n{count}";
+        }
+    }
+}
